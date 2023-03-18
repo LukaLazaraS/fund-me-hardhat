68ec94ec53423e9194d198969c1d606a53317098
LukaLazarashvili <Lazarashvili02@gmail.com>
2023-03-18 23:49:30 +0400

    add base variables into deploy ts

--- deploy/01-deploy-fund-me.ts
+++ deploy/01-deploy-fund-me.ts
@@ -1,3 +1,8 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-export default (hre: HardhatRuntimeEnvironment) => {};
+export default (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+};
