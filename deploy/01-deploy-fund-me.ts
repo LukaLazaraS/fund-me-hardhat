3161ad2e218c55e08c29eb9665ddc83269488d98
LukaLazarashvili <lazarashvili02@gmail.com>
2023-03-28 23:42:37 +0400

    update deploy script

--- deploy/01-deploy-fund-me.ts
+++ deploy/01-deploy-fund-me.ts
@@ -1,6 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-export default (hre: HardhatRuntimeEnvironment) => {
+module.exports = async (hre: HardhatRuntimeEnvironment) => {
   const { deployments, getNamedAccounts, network } = hre;
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
