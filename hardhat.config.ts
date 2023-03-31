dda87aee48c70d07e8b03cbdad8f3e5a3876875b
LukaLazarashvili <lazarashvili02@gmail.com>
2023-03-31 22:25:05 +0400

    add hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,22 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
 import "hardhat-deploy";
 
+const GOERLY_RPC_URL = process.env.GOERLY_RPC_URL!;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
   namedAccounts: {
     deployer: {
       default: 0,
